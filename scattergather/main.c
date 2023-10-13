15d5438fd2383418fa7258f8154c6728943d7b07
shalabala <utamas32@gmail.com>
2023-10-13 09:26:16 +0200

    scatter gather exercise added, gitignore suplemented

---
+++ scattergather/main.c
@@ -0,0 +1,24 @@
+#include <sys/uio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_OF_BUFFERS 5
+#define SIZE_OF_BUFFERS 5
+int main()
+{
+    char *buffers[NUM_OF_BUFFERS];
+    struct iovec vectors[NUM_OF_BUFFERS];
+    for (size_t i = 0; i < NUM_OF_BUFFERS; i++)
+    {
+        buffers[i] = calloc(SIZE_OF_BUFFERS, sizeof(char));
+        vectors[i].iov_base = buffers[i];
+        vectors[i].iov_len = SIZE_OF_BUFFERS;
+    }
+    readv(STDIN_FILENO, vectors, NUM_OF_BUFFERS);
+
+    writev(STDOUT_FILENO, vectors, NUM_OF_BUFFERS);
+    for (size_t i = 0; i < NUM_OF_BUFFERS; ++i)
+    {
+        free(buffers[i]);
+    }
+}
